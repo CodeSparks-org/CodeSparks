539fb5415dee954b7ed1af6cba7f070a23ba78b8
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-07-13 21:06:58 -0700

    Change Dockerfile to because of deployment issues Playing with styles

--- CodeSparks/Program.cs
+++ CodeSparks/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var hostingProvider = Environment.GetEnvironmentVariable("HOSTING_PROVIDER");
+bool isRender = hostingProvider?.ToLower() == "render.com";
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -13,8 +15,14 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.Logger.LogInformation("Application starting up");
+if (isRender)
+{
+    app.Logger.LogInformation("Deployed on render.com");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,20 +32,25 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    if (!isRender)
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 }
 
-app.UseHttpsRedirection();
+if (!isRender) {
+    app.UseHttpsRedirection();
+}
+    
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+app.MapHealthChecks("/health");
 
 app.Run();
