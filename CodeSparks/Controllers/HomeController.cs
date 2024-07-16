fb7cc80c0d401d10153192d2079f8a5ab8cc731e
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-07-16 07:16:36 -0700

    Playing with styles

--- CodeSparks/Controllers/HomeController.cs
+++ CodeSparks/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CodeSparks.Controllers
             return View();
         }
 
+        public IActionResult CreateSpark()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
