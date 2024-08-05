ac3077221719cf5ee3808e10928a90eb97c70979
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-08-04 20:40:24 -0700

    Add people to the nav menu

---
+++ CodeSparks/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using CodeSparks.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeSparks.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly ILogger<HomeController> _logger;
+
+        public PeopleController(AppDbContext context, ILogger<HomeController> logger)
+        {
+            _db = context;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _db.Users.ToListAsync();
+
+            return View(model);
+        }
+    }
+}
