0d6122896c11fedf4d9e08a8f95fac066cdb8964
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-08-26 21:04:59 -0700

    Move out SparkStatus to a separate entity - SparkUserStatus

---
+++ CodeSparks/Data/Models/SparkUserStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeSparks.Data.Models
+{
+    public class SparkUserStatus
+    {
+        public Guid Id { get; set; }
+
+
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+        public virtual AppUser? User { get; set; }
+
+
+        [ForeignKey("Spark")]
+        public Guid SparkId { get; set; }
+        public virtual Spark? Spark { get; set; }
+
+
+        public SparkStatus Status { get; set; } = SparkStatus.NotStarted;
+
+        public DateTime? StartedDate { get; set; }
+
+        public DateTime? CompletedDate { get; set; }
+
+        public DateTime? ReviewDate { get; set; }
+
+        
+        [ForeignKey("ReviewedBy")]
+        public Guid? ReviewedById { get; set; }
+        public virtual AppUser? ReviewedBy { get; set; }
+    }
+
+    public enum SparkStatus
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        InReview = 2,
+        Completed = 3,
+        Failed = 4,
+    }
+}
