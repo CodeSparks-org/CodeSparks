0f649ba8f4f0209a8ac3d925db90301fec77ee26
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-09-07 12:46:09 -0700

    Move HashtagList to separete entity - SparkInput

---
+++ CodeSparks/ViewModels/SparkInputViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CodeSparks.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeSparks.ViewModels
+{
+    public class SparkInput
+    {
+        [MaxLength(100)]
+        public required string Name { get; set; }
+
+        [MaxLength(1000)]
+        public required string Description { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public SparkCategory Category { get; set; }
+
+        [Url]
+        [MaxLength(250)]
+        public string? Url { get; set; }
+
+        public string HashtagList { get; set; } = string.Empty;
+    }
+}
